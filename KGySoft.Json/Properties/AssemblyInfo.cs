c8b5766f89b334cd7c6acb94956039a43b36c9cd
György Kőszeg <koszeggy@gmail.com>
2021-11-17 21:05:09 +0100

    Releasing version v2.0.0

--- KGySoft.Json/Properties/AssemblyInfo.cs
+++ KGySoft.Json/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Security;
 
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-rc.1")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
 
 [assembly: AllowPartiallyTrustedCallers]
 
